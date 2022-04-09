5684c91b49f56c85fc7abbfab06bbd942efc1b4c
zaynababaas <amntors019@gmail.com>
2022-04-10 01:22:54 +0430

    sixth step

--- controller/Api/FlightController.php
+++ controller/Api/FlightController.php
@@ -267,4 +267,177 @@ class FlightController extends BaseController
             );
         }
     }
+    //Reserve Flight flight/api7?token=123&originCity=Tehran&originAirport=THRA&destCity=Stokholms&destAirport=STOC&flightDate=2021-10-29_19:00&class=A
+    public function api7()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') 
+        {
+            try {
+                $flightModel = new FlightModel();
+                $airport = -1;
+                $originCity = (isset($arrQueryStringParams['originCity']))?$arrQueryStringParams['originCity']:-1;
+                $originAirport = (isset($arrQueryStringParams['originAirport']))?$arrQueryStringParams['originAirport']:-1;
+                $destCity = (isset($arrQueryStringParams['destCity']))?$arrQueryStringParams['destCity']:-1;
+                $destAirport = (isset($arrQueryStringParams['destAirport']))?$arrQueryStringParams['destAirport']:-1;
+                $flightDate = (isset($arrQueryStringParams['flightDate']))?$arrQueryStringParams['flightDate']:-1;
+                $class = (isset($arrQueryStringParams['class']))?$arrQueryStringParams['class']:-1;
+                $token = (isset($arrQueryStringParams['token']))?$arrQueryStringParams['token']:-1;
+                $arrUsers = $flightModel->reserveFlight($token,$originCity,$originAirport,$destCity,$destAirport,$flightDate,$class);
+                $responseData = json_encode($arrUsers);
+            } 
+            catch (Error $e) 
+            {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } 
+        else 
+        {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) 
+        {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } 
+        else 
+        {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+    //Show User Flight flight/api8?token=123&stat=showreserve
+    public function api8()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') 
+        {
+            try {
+                $flightModel = new FlightModel();
+                $stat = (isset($arrQueryStringParams['stat']))?$arrQueryStringParams['stat']:-1;
+                $token = (isset($arrQueryStringParams['token']))?$arrQueryStringParams['token']:-1;
+                $arrUsers = $flightModel->showUserReserve($token,$stat);
+                $responseData = json_encode($arrUsers);
+            } 
+            catch (Error $e) 
+            {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } 
+        else 
+        {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) 
+        {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } 
+        else 
+        {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+    //Delete User Flight flight/api9?token=123&id=2
+    public function api9()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') 
+        {
+            try {
+                $flightModel = new FlightModel();
+                $id = (isset($arrQueryStringParams['id']))?$arrQueryStringParams['id']:-1;
+                $token = (isset($arrQueryStringParams['token']))?$arrQueryStringParams['token']:-1;
+                $arrUsers = $flightModel->deleteUserReserve($token,$id);
+                $responseData = json_encode($arrUsers);
+            } 
+            catch (Error $e) 
+            {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } 
+        else 
+        {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) 
+        {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } 
+        else 
+        {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+    //signin User flight/api10?username=habibi
+    public function api10()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        if (strtoupper($requestMethod) == 'GET') 
+        {
+            try {
+                $flightModel = new FlightModel();
+                $username = (isset($arrQueryStringParams['username']))?$arrQueryStringParams['username']:-1;
+                $arrUsers = $flightModel->signinUser($username);
+                $responseData = json_encode($arrUsers);
+            } 
+            catch (Error $e) 
+            {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } 
+        else 
+        {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+ 
+        // send output
+        if (!$strErrorDesc) 
+        {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } 
+        else 
+        {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
 }
--- model/FlightModel.php
+++ model/FlightModel.php
@@ -87,4 +87,95 @@ class FlightModel extends Database
             return $userInfo;
         }
     }
+    public function reserveFlight($token,$originCity,$originAirport,$destCity,$destAirport,$flightDate,$class)
+    {
+        $userInfo = array();
+        $tmp = array();
+        $msg = 'Error';
+        $userInfo = $this->select("select * from `users` where `user_token` = '$token' ",$tmp,2);
+        if (count($userInfo) > 0)
+        {
+            $flightStat = $this->select("select `id`,`plane_type` from `flight` where `origin_city` LIKE '$originCity' AND `origin_airport` LIKE '$originAirport' 
+            AND `dest_city` LIKE '$destCity' AND `dest_airport` LIKE '$destAirport' AND `datetime` = '$flightDate' AND `class` LIKE '$class' ",$tmp,1);
+            if (count($flightStat) > 0)
+            {
+                $flightId = $flightStat[0]['id'];
+                $plane_type = $flightStat[0]['plane_type'];
+                $flightRes = $this->select("select count(`id`) as `cid` from `reserve` where `flight_id` = '$flightId'",$tmp,1);
+                $reserved = $flightRes[0]['cid']+1;
+                $flightCap = $this->select("select `capecity` from `plane` where `plane_name` = '$plane_type'",$tmp,1);
+                $capecity = $flightCap[0]['capecity'];
+                if ($reserved < $capecity)
+                {
+                    $capecity--;
+                    $msg = 'ok';
+                    $reserve = $this->executeStatement("insert into `reserve`(`user_token`, `flight_id`) VALUES ('$token','$flightId')",$tmp,1);
+                    $reserve = $this->executeStatement("update `plane` SET `capecity`='$capecity' WHERE `plane_name` = '$plane_type' ",$tmp,1);
+                    return $msg;
+                }
+            }
+            else
+            {
+                echo 'nok';
+            }
+        }
+        else
+        {
+            return $userInfo;
+        }
+    }
+    public function showUserReserve($token,$stat)
+    {
+        $userInfo = array();
+        $tmp = array();
+        $userInfo = $this->select("select * from `users` where `user_token` = '$token' ",$tmp,2);
+        if ((count($userInfo) > 0) && ($stat == 'showreserve'))
+        {
+            return $this->select("select * from `reserve` LEFT JOIN flight on `flight_id` = flight.id WHERE `user_token` = '$token' ",$tmp,1);
+        }
+        else
+        {
+            return $userInfo;
+        }
+    }
+    public function deleteUserReserve($token,$id)
+    {
+        $userInfo = array();
+        $tmp = array();
+        $msg = 'Error';
+        $userInfo = $this->select("select * from `users` where `user_token` = '$token' ",$tmp,2);
+        if (count($userInfo) > 0)
+        {
+            $msg = 'ok';
+            $this->executeStatement("delete from `reserve` WHERE `flight_id` = '$id' and `user_token` = '$token' ",$tmp,1);
+            $flightInfo = $this->select("select `plane_type` from `flight` where `id`='$id' ",$tmp,1);
+            $plane_type = $flightInfo[0]['plane_type'];
+            if (count($flightInfo) > 0)
+                $reserve = $this->executeStatement("update `plane` SET `capecity`=capecity+1 WHERE `plane_name` = '$plane_type' ",$tmp,1);
+            return $msg;
+        }
+        else
+        {
+            return $userInfo;
+        }
+    }
+    public function signinUser($userName)
+    {
+        $userInfo = array();
+        $tmp = array();
+        $msg = 'User Exist';
+        $userInfo = $this->select("select * from `users` where `user_name` = '$userName' ",$tmp,2);
+        if (count($userInfo) <= 0)
+        {
+            $msg = 'ok';
+            $rnd = rand()*245;
+            $token = $userName.$rnd;
+            $this->executeStatement("insert into `users`(`user_name`, `user_email`, `user_token`) VALUES ('$userName','$userName','$token') ",$tmp,2);
+            return $token;
+        }
+        else
+        {
+            return $msg;
+        }
+    }
 }
